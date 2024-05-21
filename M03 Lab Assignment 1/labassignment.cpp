e64eb929c20d3cbf7a754ac58134319fefe40a62
rh25170 <rh25170@gmail.com>
2024-05-21 18:58:45 -0400

    Pushing all files related to this

---
+++ M03 Lab Assignment 1/labassignment.cpp
@@ -0,0 +1,43 @@
+/* Filename: labassignment.cpp
+* Author: Ryan Hurd
+* Date: 1/31/2018
+* Purpose: Output and ASCII C++ logo
+*/
+
+/* I actually had quite a bit of fun with this assignment.
+*  Looking forward to more!
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "    **********************************" << endl;
+	cout << "    *      M03 Lab Assignment 1      *" << endl;
+	cout << "    *     Computer Programming I     *" << endl;
+	cout << "    *        Author: Ryan Hurd       *" << endl;
+	cout << "    *   Due Date: February 8th, 2018 *" << endl;
+	cout << "    **********************************" << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << "    CCCCCCCCC     ++           ++" << endl;
+	cout << "    CC            ++           ++" << endl;
+	cout << "    CC       +++++++++++   +++++++++++" << endl;
+	cout << "    CC       +++++++++++   +++++++++++" << endl;
+	cout << "    CC            ++           ++" << endl;
+	cout << "    CCCCCCCCC     ++           ++" << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << "        C++ is the bomb-digity" << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+
+
+	system("pause");
+	return 0;
+}
